7266c02558739e728f16bb526d935adf902b5a63
ashleyf1996 <ashleyf1996@gmail.com>
2017-03-19 23:21:02 +0000

    setup my database

--- entertainment_tonight/models.py
+++ entertainment_tonight/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# here are all of my models
+
+class Cities(models.Model):
+    town_name = models.CharField(max_length=200)
+    town_picture = models.CharField(max_length=400)
+
+
