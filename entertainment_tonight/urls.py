e3e12f7f497d67ea88cd1dd887052e25259f14f1
ashleyf1996 <ashleyf1996@gmail.com>
2017-03-20 21:35:58 +0000

    made another view

--- entertainment_tonight/urls.py
+++ entertainment_tonight/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /entertainment_tonight/
     url(r'^$', views.index, name='index'),
+
+    # /entertainment_tonight/info if someone writes 1 it saves it in s var called city id. 1-9+ this patt mathces any following ints
+    url(r'^(?P<cities_id>[0-9]+)/$', views.detail,name='detail'),
 ]
--- entertainment_tonight/views.py
+++ entertainment_tonight/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Cities
 
 
 def index(request):
     template = loader.get_template('index.html')
-    return HttpResponse(template.render(request))
+    html = ''
+    all_cities = Cities.objects.all()
+    for city in all_cities:
+        url = '/entertainment_tonight/' + str(city.id) + '/'
+        html += '<a href="' + url +'">' + city.town_name + '</a> <br>'
+    return HttpResponse(template.render(request), html)
+
+
+def detail(request, cities_id):
+    return HttpResponse("<h2>Details for city id: "+ str(cities_id) + "</h2>")
 
 
