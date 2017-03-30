9a87a8473c05019458108464e5285b579cb379c7
ashleyf1996 <ashleyf1996@gmail.com>
2017-03-30 09:54:11 +0100

    allowed the user to login

--- entertainment_tonight/urls.py
+++ entertainment_tonight/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from . import views
-
+from accounts.views import (login_view)
 urlpatterns = [
     # /entertainment_tonight/
     url(r'^$', views.index, name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # /entertainment_tonight/info if someone writes 1 it saves it in s var called city id. 1-9+ this patt mathces any following ints
 
-    url(r'^create/$', views.home, name='create')
+    url(r'^create/$', views.home, name='create'),
+    url(r'^login/$', login_view, name='login'),
 ]
 
