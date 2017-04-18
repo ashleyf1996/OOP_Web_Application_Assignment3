e8fbc012181c8eb0c6d503366a8e2a2b95847e77
ashleyf1996 <ashleyf1996@gmail.com>
2017-04-18 14:56:43 +0100

    made my logo a link to the home page

--- entertainment_tonight/static/entertainment_tonight/form.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
-from entertainment_tonight.models import Event
-
-
-class EventCreate(CreateView):
-    # trying to create a new event
-    model = Event
-    fields = ['event_name', 'event_location', 'event_type', 'upload_photo']
